52c171263c057a54c338687c77291bbef6b8536d
Rafael <rafaelmeirelles8@gmail.com>
2022-03-07 14:07:36 -0400

    make test fail.

--- src/test/java/com/fullstack/completeFullStackJavaReact/CompleteFullStackJavaReactApplicationTests.java
+++ src/test/java/com/fullstack/completeFullStackJavaReact/CompleteFullStackJavaReactApplicationTests.java
@@ -1,6 +1,5 @@
 package com.fullstack.completeFullStackJavaReact;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +8,6 @@ class CompleteFullStackJavaReactApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Assertions.fail("test failed");
 	}
 
 }

627df704f34a41327b0a634b7298955fcfe498a9
Rafael <rafaelmeirelles8@gmail.com>
2022-03-18 14:46:07 -0300

    edit feature added

--- src/frontend/src/client.js
+++ src/frontend/src/client.js
@@ -15,6 +15,10 @@ export const getAllStudents = () =>
     fetch("api/v1/students/")
     .then(checkStatus);
 
+export const getStudentById = (studentId) =>
+    fetch("api/v1/students/" + studentId)
+        .then(checkStatus);
+
 export const createStudent = student =>
     fetch("api/v1/students/", {
         headers: {
@@ -23,7 +27,18 @@ export const createStudent = student =>
         method: 'POST',
         body: JSON.stringify(student)
      }
-    ).then(checkStatus);;
+    ).then(checkStatus);
+
+export const updateStudent = student =>
+    fetch("api/v1/students/", {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'PUT',
+        body: JSON.stringify(student)
+     }
+    ).then(checkStatus);
+
 
 export const deleteStudent = student =>
     fetch("api/v1/students/", {
